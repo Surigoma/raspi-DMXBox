e63822c1ca8bb5439839f9a81ca4c204372b97ea
Surigoma <negi.1994.t.taku.1216@gmail.com>
2022-09-01 16:17:08 +0100

    Support TCP server (2/2)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from rest_api import start_restapi
 from dmx_sender import start_dmx
 from serial_port import start_serial
+from tcp_server import start_tcp
 from multiprocessing import Process, Pipe, Array
 from config import config
 import json
@@ -13,9 +14,10 @@ def logmsg(message): print("[main]" + message)
 pipe_dmx_p = None
 pipe_rest_p = None
 pipe_serial_p = None
+pipe_tcp_p = None
 
 def transportMessage(message):
-    global pipe_dmx_p, pipe_rest_p, pipe_serial_p
+    global pipe_dmx_p, pipe_rest_p, pipe_serial_p, pipe_tcp_p
     if not "to" in message and "body" in message:
         logmsg("unknown message." + json.dumps(message))
     elif pipe_dmx_p != None and message["to"] == "dmx":
@@ -24,6 +26,8 @@ def transportMessage(message):
         pipe_rest_p.send(message["body"])
     elif pipe_serial_p != None and message["to"] == "serial":
         pipe_serial_p.send(message["body"])
+    elif pipe_tcp_p != None and message["to"] == "tcp":
+        pipe_tcp_p.send(message["body"])
     return
 
 if __name__ == '__main__':
@@ -40,9 +44,13 @@ if __name__ == '__main__':
     pipe_serial_p, pipe_serial_c = Pipe()
     p_serial = Process(target=start_serial, args=(pipe_serial_c, config_data, ))
 
+    pipe_tcp_p, pipe_tcp_c = Pipe()
+    p_tcp = Process(target=start_tcp, args=(pipe_tcp_c, config_data, ))
+
     p_dmx.start()
     p_rest.start()
     p_serial.start()
+    p_tcp.start()
     running = True
 
     while running:
@@ -55,12 +63,17 @@ if __name__ == '__main__':
         if pipe_serial_p.poll():
             message = pipe_serial_p.recv()
             transportMessage(message)
+        if pipe_tcp_p.poll():
+            message = pipe_tcp_p.recv()
+            transportMessage(message)
         time.sleep(0.01)
 
     p_dmx.join()
     p_rest.join()
     p_serial.join()
+    p_tcp.join()
     
     pipe_dmx_p.close()
     pipe_rest_p.close()
     pipe_serial_p.close()
+    pipe_tcp_p.close()
