5831a14ac525d916c407626cb6fb497525ae27ab
Surigoma <negi.1994.t.taku.1216@gmail.com>
2025-09-10 13:33:13 +0000

    Refactoring a main.py

--- main.py
+++ main.py
@@ -20,83 +20,69 @@ pipe_rest_p = None
 pipe_tcp_p = None
 pipe_osc_p = None
 
+pipe_table: Dict[str, Connection] = {
+    "dmx": None,
+    "rest": None,
+    "serial": None,
+    "tcp": None,
+    "osc": None,
+}
+process_table: Dict[str, Process] = {
+    "dmx": None,
+    "rest": None,
+    "serial": None,
+    "tcp": None,
+    "osc": None,
+}
+
 ignoreRemote = False
+running = True
 
 def transportMessage(message: Dict[str, Dict]):
-    global pipe_dmx_p, pipe_rest_p, pipe_serial_p, pipe_tcp_p, pipe_osc_p, ignoreRemote
+    global pipe_table
     if not "to" in message and "body" in message:
         logmsg("unknown message." + json.dumps(message))
-    elif pipe_dmx_p != None and message["to"] == "dmx":
-        pipe_dmx_p.send(message["body"])
-    elif pipe_rest_p != None and message["to"] == "rest":
-        pipe_rest_p.send(message["body"])
-    #elif pipe_serial_p != None and message["to"] == "serial":
-    #    pipe_serial_p.send(message["body"])
-    elif pipe_tcp_p != None and message["to"] == "tcp":
-        pipe_tcp_p.send(message["body"])
-    elif pipe_osc_p != None and message["to"] == "osc":
-        pipe_osc_p.send(message["body"])
+    elif message["to"] in pipe_table and pipe_table[message["to"]] is not None:
+        pipe_table[message["to"]].send(message["body"])
     elif message["to"] == "main":
         if message["body"]["method"] == "ignoreRemote":
             ignoreRemote = bool(message["body"]["param"])
             logmsg("ignore Remote :" + str(ignoreRemote) + " " + str(message["body"]["param"]))
     return
 
+def register_pipelines(name: str, target: Callable[[Connection, config], None], config: config):
+    global process_table, pipe_table
+    p, c = Pipe()
+    pipe_table[name] = p
+    process_table[name] = Process(target=target, args=(c, config, ))
+
 if __name__ == '__main__':
     config_data = config()
 
-    # Create DMX process server
-    pipe_dmx_p, pipe_dmx_c = Pipe()
-    p_dmx = Process(target=start_dmx, args=(pipe_dmx_c, config_data, ))
+    register_pipelines("dmx", start_dmx, config_data) # Create DMX process server
+    register_pipelines("rest", start_restapi, config_data) # Create REST API and static file server
+    # register_pipelines("serial", start_serial, config_data)
+    register_pipelines("tcp", start_tcp, config_data)
+    register_pipelines("osc", start_osc, config_data)
 
-    # Create REST API and static file server
-    pipe_rest_p, pipe_rest_c = Pipe()
-    p_rest = Process(target=start_restapi, args=(pipe_rest_c, config_data, ))
-
-    #pipe_serial_p, pipe_serial_c = Pipe()
-    #p_serial = Process(target=start_serial, args=(pipe_serial_c, config_data, ))
-
-    pipe_tcp_p, pipe_tcp_c = Pipe()
-    p_tcp = Process(target=start_tcp, args=(pipe_tcp_c, config_data, ))
-
-    pipe_osc_p, pipe_osc_c = Pipe()
-    p_osc = Process(target=start_osc, args=(pipe_osc_c, config_data, ))
-
-    p_dmx.start()
-    p_rest.start()
-    #p_serial.start()
-    p_tcp.start()
-    p_osc.start()
-    running = True
+    for k, v in process_table.items():
+        if v is not None:
+            v.start()
 
     while running:
-        if pipe_rest_p.poll():
-            message = pipe_rest_p.recv()
-            transportMessage(message)
-        if pipe_dmx_p.poll():
-            message = pipe_dmx_p.recv()
-            transportMessage(message)
-        #if pipe_serial_p.poll():
-        #    message = pipe_serial_p.recv()
-        #    if ignoreRemote == False:
-        #        transportMessage(message)
-        if pipe_tcp_p.poll():
-            message = pipe_tcp_p.recv()
-            if ignoreRemote == False:
-                transportMessage(message)
-        if pipe_osc_p.poll():
-            message = pipe_osc_p.recv()
-            transportMessage(message)
+        try:
+            for k, v in pipe_table.items():
+                if isinstance(v, Connection):
+                    if v.poll():
+                        transportMessage(v.recv())
+        except KeyboardInterrupt:
+            running = False
         time.sleep(0.01)
 
-    p_dmx.join()
-    p_rest.join()
-    p_serial.join()
-    p_tcp.join()
-    p_osc.join()
-    
-    pipe_dmx_p.close()
-    pipe_rest_p.close()
-    #pipe_serial_p.close()
-    pipe_tcp_p.close()
-    pipe_osc_p.close()
+    for k, v in pipe_table.items():
+        if isinstance(v, Connection):
+            v.close()
+
+    for k, v in process_table.items():
+        if v is not None:
+            v.join()
